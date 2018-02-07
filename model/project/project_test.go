7eaf1b4129adda4d768a8f085002f221cca5c46b
marthjod@gmail.com <marthjod@gmail.com>
2018-02-07 11:40:03 +0100

    add project_test

---
+++ model/project/project_test.go
@@ -0,0 +1,37 @@
+package project
+
+import (
+	"reflect"
+	"testing"
+)
+
+var expected = []struct {
+	projectStr []string
+	projects   []Project
+}{
+	{
+		projectStr: []string{},
+		projects:   []Project{},
+	},
+	{
+		projectStr: []string{"+__", "+--", "+@"},
+		projects:   []Project{"__", "--", "@"},
+	},
+	{
+		projectStr: []string{"+Foo"},
+		projects:   []Project{"Foo"},
+	},
+	{
+		projectStr: []string{"+Foo", "+Bar"},
+		projects:   []Project{"Foo", "Bar"},
+	},
+}
+
+func TestGetProjects(t *testing.T) {
+	for _, exp := range expected {
+		actual := GetProjects(exp.projectStr...)
+		if !reflect.DeepEqual(actual, exp.projects) {
+			t.Errorf("Expected %+v, got %+v", exp.projects, actual)
+		}
+	}
+}
